81e75f1351e3dd7cb3f5ebc740dcc18e9c26f02c
devfer-js <fernandoquinterosgutierrez@gmail.com>
2018-11-29 12:37:02 -0400

    version final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', pathMatch: 'full', redirectTo: 'home' },
+{path: 'home', component: HomeComponent},
+{path: 'heroes', component: HeroesComponent},
+{path: 'about', component: AboutComponent},
+{path: 'heroe/:id', component: HeroeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
