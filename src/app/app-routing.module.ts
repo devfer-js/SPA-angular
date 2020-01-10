c3a0a79059f3d99411b5140ebb31d12ccf34cb11
devfer <ferkki@hotmail.com>
2020-01-10 09:33:48 -0400

    ready for production build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
-import { AboutComponent } from './components/about/about.component';
-import { HeroeComponent } from './components/heroe/heroe.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { HeroesComponent } from "./components/heroes/heroes.component";
+import { AboutComponent } from "./components/about/about.component";
+import { HeroeComponent } from "./components/heroe/heroe.component";
 
 const routes: Routes = [
-{ path: '', pathMatch: 'full', redirectTo: 'home' },
-{path: 'home', component: HomeComponent},
-{path: 'heroes', component: HeroesComponent},
-{path: 'about', component: AboutComponent},
-{path: 'heroe/:id', component: HeroeComponent}
+  { path: "", pathMatch: "full", redirectTo: "home" },
+  { path: "home", component: HomeComponent },
+  { path: "heroes", component: HeroesComponent },
+  { path: "about", component: AboutComponent },
+  { path: "heroe/:id", component: HeroeComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
